799c7b93f4ed4b9c5c0f848bcd07117e8046d9b0
gleysonsilvino <gleysonsilvino@gmail.com>
2020-11-06 18:30:11 -0300

    finalizado desafio-05

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,23 +6,51 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private balance: Balance;
+
   constructor() {
     this.transactions = [];
+    this.balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
   }
 
   public all(): Transaction[] {
     // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
     // TODO
+    return this.balance;
   }
 
-  public create(): Transaction {
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
     // TODO
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    if (type === 'income') {
+      this.balance.income += value;
+    } else if (type === 'outcome') {
+      this.balance.outcome += value;
+    } else throw Error('Invalid type');
+
+    this.balance.total = this.balance.income - this.balance.outcome;
+
+    return transaction;
   }
 }
 
